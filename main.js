6a873167dcd4ca5d50ff4bb48ad9205069925756
eliflea <leyla.altay@live.se>
2024-09-27 13:10:58 +0200

    hamburgare

---
+++ main.js
@@ -0,0 +1,8 @@
+const hamMenu = document.querySelector('.hamburger');
+
+const offScreenNav = document.querySelector('.off-screen-navigation');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenNav.classList.toggle('active');
+})
