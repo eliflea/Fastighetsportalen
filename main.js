b29dc537f61e54799fde47f377db7098015f0090
eliflea <leyla.altay@live.se>
2024-09-29 02:38:15 +0200

    bubbla, kontaktform

--- main.js
+++ main.js
@@ -6,5 +6,23 @@ const offScreenNav = document.querySelector('.off-screen-navigation');
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   offScreenNav.classList.toggle('active');
-})
 
+  // if (offScreenNav.classList.contains('active')) {
+  //   hamMenu.classList.add('fixed');
+  // } else {
+  //   hamMenu.classList.remove('fixed');
+  // }
+});
+
+//kontakt från startsidan
+const contactLogo = document.getElementById('hjalp-logo');
+const contactBox = document.getElementById('kontakt');
+const closeContact = document.getElementById('kontakt-stang');
+
+contactLogo.addEventListener('click', () => {
+  contactBox.style.display = contactBox.style.display === 'block' ? 'none' : 'block'; // Toggle display
+});
+
+closeContact.addEventListener('click', () => {
+  contactBox.style.display = 'none'; // Hide the contact box
+});
